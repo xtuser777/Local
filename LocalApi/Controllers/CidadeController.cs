74cc42f7f770be39b3fe3fe92c3d850057531604
Lucas <lucaoxt@gmail.com>
2020-05-16 15:01:21 -0300

    Terceiro commit

---
+++ LocalApi/Controllers/CidadeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using LocalLib.Controls;
+using LocalLib.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LocalApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CidadeController : ControllerBase
+    {
+        private readonly ILogger<EstadoController> _logger;
+
+        public CidadeController(ILogger<EstadoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{id}")]
+        public List<Cidade> Get(int id)
+        {
+            var cidades = new CidadeControl().ObterPorEstado(id);
+
+            return cidades;
+        }
+    }
+}
---
+++ LocalApi/Controllers/EstadoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using LocalLib.Controls;
+using LocalLib.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LocalApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EstadoController : ControllerBase
+    {
+        private readonly ILogger<EstadoController> _logger;
+
+        public EstadoController(ILogger<EstadoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<Estado> Get()
+        {
+            var estados = new EstadoControl().ObterTudo();
+
+            return estados;
+        }
+    }
+}
